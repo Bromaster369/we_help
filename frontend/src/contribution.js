bb38270cf19341bb9a5041918972c2bad65dcc5b
Bromaster369 <chrispardopersonal@gmail.com>
2021-04-23 13:03:04 -0400

    created frontend js models and DOM

---
+++ frontend/src/contribution.js
@@ -0,0 +1,10 @@
+class Contribution{
+    constructor(title, description){
+        this.title = title;
+        this.description = description;
+    }
+
+    // instance method that's going to render the object to the DOM
+    
+}
+
---
+++ frontend/src/user.js
@@ -0,0 +1,10 @@
+class User{
+    constructor(name, username, email){
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
+
+    //render user instance method
+
+}
